6af39209fbd08f2e7665af2c8d55103869e4b2ed
haruBlank0-0 <harublank00@gmail.com>
2026-06-01 17:57:17 +0545

    feat: add hero banner on men page

--- .adonisjs/server/pages.d.ts
+++ .adonisjs/server/pages.d.ts
@@ -14,12 +14,12 @@ declare module '@adonisjs/inertia/types' {
   export interface InertiaPages {
     'auth/login': ExtractProps<(typeof import('../../inertia/pages/auth/login.tsx'))['default']>
     'auth/signup': ExtractProps<(typeof import('../../inertia/pages/auth/signup.tsx'))['default']>
-    'errors/not_found': ExtractProps<(typeof import('../../inertia/pages/errors/not_found.tsx'))['default']>
-    'errors/server_error': ExtractProps<(typeof import('../../inertia/pages/errors/server_error.tsx'))['default']>
-    'home/index': ExtractProps<(typeof import('../../inertia/pages/home/index.tsx'))['default']>
-    'home/components/hero/index': ExtractProps<(typeof import('../../inertia/pages/home/components/hero/index.tsx'))['default']>
     'commerce/men': ExtractProps<(typeof import('../../inertia/pages/commerce/men.tsx'))['default']>
     'commerce/women': ExtractProps<(typeof import('../../inertia/pages/commerce/women.tsx'))['default']>
+    'errors/not_found': ExtractProps<(typeof import('../../inertia/pages/errors/not_found.tsx'))['default']>
+    'errors/server_error': ExtractProps<(typeof import('../../inertia/pages/errors/server_error.tsx'))['default']>
     'home/components/hero/hero_panel': ExtractProps<(typeof import('../../inertia/pages/home/components/hero/hero_panel.tsx'))['default']>
+    'home/components/hero/index': ExtractProps<(typeof import('../../inertia/pages/home/components/hero/index.tsx'))['default']>
+    'home/index': ExtractProps<(typeof import('../../inertia/pages/home/index.tsx'))['default']>
   }
 }
--- inertia/pages/commerce/men.tsx
+++ inertia/pages/commerce/men.tsx
@@ -1,12 +1,36 @@
 import type { InertiaProps } from '~/types'
-import type { Data } from '@generated/data'
+import { Link } from '@adonisjs/inertia/react'
+import MenShoes from '~/assets/men/shoes.jpg'
+import MaleModel from '~/assets/men/male-model.jpg'
+import { Button } from '~/components/ui/button'
+import { LucideMoveRight } from 'lucide-react'
 
 type PageProps = InertiaProps<{}>
 
 export default function Men({}: PageProps) {
   return (
-    <div>
-      <h1>Men</h1>
+    <div className="">
+      <Link route="men">
+        <div className="flex h-[80vh] overflow-hidden relative">
+          <figure className="flex-1">
+            <div className="" />
+            <img src={MaleModel} alt="Stylish model" className="block object-fill h-full w-full" />
+          </figure>
+
+          <figure className="flex-1 relative">
+            <img src={MenShoes} alt="Stylish shoes" className="block object-full h-full w-full" />
+          </figure>
+
+          <Button className="group absolute right-8 bottom-8 z-10 flex items-center overflow-hidden px-8 py-6 text-2xl capitalize opacity-90 transition-opacity duration-300 hover:opacity-100 cursor-pointer">
+            <span>shop now</span>
+
+            <span className="flex w-0 items-center overflow-hidden opacity-0 transition-all duration-300 ease-out group-hover:ml-3 group-hover:w-7 group-hover:opacity-100">
+              <LucideMoveRight className="h-7 w-7" />
+            </span>
+          </Button>
+          <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-transparent" />
+        </div>
+      </Link>
     </div>
   )
 }
