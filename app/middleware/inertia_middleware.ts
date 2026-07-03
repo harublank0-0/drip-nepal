a18610f3f59c4882f301083b8ce4ae1cb338985e
haruBlank0-0 <harublank00@gmail.com>
2026-07-03 14:01:17 +0545

    feat: return shops in shared props if user has shops

--- app/middleware/inertia_middleware.ts
+++ app/middleware/inertia_middleware.ts
@@ -2,9 +2,10 @@ import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 import UserTransformer from '#transformers/user_transformer'
 import BaseInertiaMiddleware from '@adonisjs/inertia/inertia_middleware'
+import ShopTransformer from '#transformers/shop_transformer'
 
 export default class InertiaMiddleware extends BaseInertiaMiddleware {
-  share(ctx: HttpContext) {
+  async share(ctx: HttpContext) {
     /**
      * The share method is called everytime an Inertia page is rendered. In
      * certain cases, a page may get rendered before the session middleware
@@ -25,6 +26,9 @@ export default class InertiaMiddleware extends BaseInertiaMiddleware {
      * Data shared with all Inertia pages. Make sure you are using
      * transformers for rich data-types like Models.
      */
+    const shopsQuery = (await auth?.user?.related('shops').query()) ?? []
+    const shops = ShopTransformer.transform(shopsQuery)
+
     return {
       errors: ctx.inertia.always(this.getValidationErrors(ctx)),
       flash: ctx.inertia.always({
@@ -34,6 +38,7 @@ export default class InertiaMiddleware extends BaseInertiaMiddleware {
       user: ctx.inertia.always(
         auth?.user?.related('shops') ? UserTransformer.transform(auth.user) : undefined
       ),
+      shops,
     }
   }
 
